acab85e0459dd04431220e7e19468ed5b4e74c08
Felipe Brabalho <felipebarbalho.95@gmail.com>
2016-11-27 09:44:14 -0300

    Refactoring and implements comunication between Console and Joytick

--- console/console.ino
+++ console/console.ino
@@ -1,20 +1,28 @@
 #include <LiquidCrystal.h> 
-#include <IRremote.h>
 
-#define MOVE_LEFT     0x8c22657a
-#define MOVE_RIGHT    0x8c22657b
-#define MOVE_FRONT    0x8c22657c
-#define MOVE_BACK     0x8c22657d
+
+// Define move buttons
+#define MOVE_LEFT 2
+#define MOVE_RIGHT 3
+#define MOVE_FRONT 4
+#define MOVE_BACK 5
+
+// Define rotate buttons
+#define MOVE_L_ROTATE_X 6  // Rotate axis x to left
+#define MOVE_R_ROTATE_X 7  // Rotate axis x to right
+#define MOVE_L_ROTATE_Y 8  // Rotate axis y to left
+#define MOVE_R_ROTATE_Y 9  // Rotate axis y to right
+#define MOVE_L_ROTATE_Z 10 // Rotate axis z to left
+#define MOVE_R_ROTATE_Z 11 // Rotate axis z to right
+
+char BOTTON_CODE[] = {'L','R','F','B', 'A','C','D','E','F','G'};
 
 const int RECEIVE_PIN = 8;
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
-IRrecv irrecv(RECEIVE_PIN);
-decode_results results;
 
 void setup(){
     Serial.begin(9600);
     lcd.begin(16, 2); 
-    irrecv.enableIRIn(); // Start the receiver
 }
 
 
@@ -34,49 +42,32 @@ void move_back(){
   
 }
 
-
 void loop() {
     printDisplay("teste", String(millis()/1000));
-    verifyIrReciever();
 }
 
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char) Serial.read();
 
-void verifyIrReciever(){
-    if (irrecv.decode(&results)) {
-
-      unsigned long  value = results.value;
-
-      switch(value){
-          case MOVE_LEFT:
-              move_left();
-              break;
-          case MOVE_RIGHT:
-              move_right();
-              break;
-          case MOVE_FRONT:
-              move_front();
-              break;
-          case MOVE_BACK:
-              move_back();
-              break;
-          default:
-
-              break;
-      }
-
-      
-
-      //TESTE
-      String valueText(value, HEX);
-      if(valueText != "FFFFFFFF" && valueText != "ffffffff"){
-          Serial.println(valueText);
-      }
-      /////
-
-      
-      
-      irrecv.resume(); // Receive the next value
+    switch(inChar){
+      case MOVE_LEFT:
+          move_left();
+          break;
+      case MOVE_RIGHT:
+          move_right();
+          break;
+      case MOVE_FRONT:
+          move_front();
+          break;
+      case MOVE_BACK:
+          move_back();
+          break;
+      default:
+          break;
     }
+    Serial.println(inChar);
+  }
 }
 
 void printDisplay(String text1, String text2){
